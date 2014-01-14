edff57a40538cc9f41eb59c3e73faf958fb34cf9
anandsun <anchorflow@gmail.com>
2014-01-13 18:40:29 -0600

    Create home skeleton

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+  </head>
+  <body>
+    <div class = "btn-group btn-group-horizontal">
+      <button type="button" name="about" id="about" class="btn btn-primary" />
+      <button type="button" name="math" id="math" class="btn btn-success" />
+    </div>
+    <p>Hi</p>
+  </body>
+</html>
